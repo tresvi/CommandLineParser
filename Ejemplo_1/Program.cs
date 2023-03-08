c3d08b062894af4291bd21508c85da95b632b08c
Raul Palavecino <raulpalav@gmail.com>
2023-03-08 16:28:35 -0300

    Mejora de ejemplos

--- Ejemplo_1/Program.cs
+++ Ejemplo_1/Program.cs
@@ -5,10 +5,25 @@ using System;
 
 namespace Ejemplo_1
 {
+
+    //Clase que Modela los parametros esperados
+    public class Parameters
+    {
+        [Option("archivo-salida", 'a', true, HelpText = "Nombre del archivo a crear.")]
+        public string ArchivoSalida { get; set; }
+
+        [Option("tamano", 't', true, HelpText = "Tamaño del archivo en bytes.")]
+        public int Tamano { get; set; }
+
+        [Flag("notificar", 'n', HelpText = "Indica si se debe notificar por mail el resultado del proceso.")]
+        public bool NotificarPorMail { get; set; }
+    }
+
+
     internal class Program
     {
         //Se puede invocar como:
-        //  \>Ejemplo_1.exe --archivo-entrada "C:\Temp\archivo1.txt --tamano 120 --notificar
+        //  \>Ejemplo_1.exe --archivo-salida "C:\Temp\archivo1.txt --tamano 120 --notificar
         // o bien reemplazar el nombre largo de los parametros por los cortos
         //  \>Ejemplo_1.exe -a "C:\Temp\archivo1.txt -t 120 --notificar
         static void Main(string[] args)
@@ -16,7 +31,7 @@ namespace Ejemplo_1
             try
             {
                 Parameters parametros = CommandLine.Parse<Parameters>(args);
-                Console.WriteLine($"Archivo de Entrada: {parametros.ArchivoEntrada}");
+                Console.WriteLine($"Archivo de Salida: {parametros.ArchivoSalida}");
                 Console.WriteLine($"Tamano: {parametros.Tamano}");
                 Console.WriteLine($"Notificar: {parametros.NotificarPorMail}");
                 Console.WriteLine("Fin OK!!");
@@ -26,22 +41,6 @@ namespace Ejemplo_1
                 Console.WriteLine($"ERROR: {ex.Message}");
             }
             Console.ReadKey();
-        }
-
-
-        //Clase que Modela los parametros esperados
-        public class Parameters
-        {
-            [Option("archivo-entrada", 'a', true, HelpText = "Nombre del archivo a crear.")]
-            public string ArchivoEntrada { get; set; }
-
-
-            [Option("tamano", 't', true, HelpText = "Tamaño del archivo en bytes.")]
-            public int Tamano { get; set; }
-
-
-            [Flag("notificar", 'n', HelpText = "Indica si se debe notificar por mail el resultado del proceso.")]
-            public bool NotificarPorMail { get; set; }
         }
     }
 }
